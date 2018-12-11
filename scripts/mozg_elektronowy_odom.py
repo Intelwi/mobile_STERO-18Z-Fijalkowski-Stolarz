547d34e8e4a77d25a8071365d1a2eefe98e4d997
Michał Stolarz <mstolarz@puma.robotyka.ia.pw.edu.pl>
2018-12-11 17:49:45 +0100

    odometry

--- scripts/mozg_elektronowy_odom.py
+++ scripts/mozg_elektronowy_odom.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import rospy
 import math
+import PyKDL
 from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from decimal import Decimal
 
-OMEGA = 2*math.pi/6.28 # pelny obrot na 5s
+OMEGA = 2*math.pi/40 # pelny obrot na 5s
 VELOC = 0.8 # do ustalenia doswiadczalnie
 HZ = 50
 
@@ -95,15 +98,23 @@ def listener():
 	"""
 	rospy.init_node('listener', anonymous=True)
 	rospy.Subscriber('chatter', Pose, callback) # TOPIC: chatter
+	rospy.Subscriber('/elektron/mobile_base_controller/odom',Odometry,get_navigation)
 	print "READY TO DO A JOB"
 
+def get_navigation(data):
+	#rospy.loginfo(rospy.get_caller_id() + "I got location: %s", data)
+	rot = PyKDL.Rotation.Quaternion(data.pose.pose.orientation.x ,data.pose.pose.orientation.y ,data.pose.pose.orientation.z ,data.pose.pose.orientation.w )
+	[roll,pitch,yaw] = rot.GetRot()
+	cuRoPo.theta = yaw
+	cuRoPo.x = data.pose.pose.position.x
+	cuRoPo.y = data.pose.pose.position.y	
 
 def callback(data):
 	"""
 	Odbiera wiadomosci i wywoluje funkcje zarzadzajaca
 	
 	"""
-	rospy.loginfo(rospy.get_caller_id() + "I heard an order: %s", data.position.z)
+	#rospy.loginfo(rospy.get_caller_id() + "I heard an order: %s", data.position.z)
 	manager(data.position)
 
 
@@ -115,7 +126,8 @@ def manager(data):
 	if(data.z == 0):
 		calcToPoint(data)
 		if(setRoPo.tRot != 0 or setRoPo.tGo != 0):
-			talkerToPoint()
+			print "JEST OK"
+			talkerToPoint(data)
 	'''elif(data.desc == "there_and_back"):
 		#TO DO
 	elif(data.desc == "rotating"):
@@ -150,13 +162,13 @@ def buildMess(go, rot, sign):
 	vel_msg.angular.x = 0
 	vel_msg.angular.y = 0
 	vel_msg.angular.z = OMEGA*rot*sign
-	print "predkosc obrotowa:", vel_msg.angular.z
+	#print "predkosc obrotowa:", vel_msg.angular.z
 	return vel_msg
 
 
 ###----------------------------------------v--TALKERS--v----------------------------------------###
 
-def talkerToPoint():
+def talkerToPoint(data):
 	"""
 	Publikuje polecenia do robota,
 	majace na celu jego przemieszczenie do zadanego punktu
@@ -165,44 +177,46 @@ def talkerToPoint():
 	pub = rospy.Publisher('/mux_vel_nav/cmd_vel', Twist, queue_size=10) # TOPIC: /mux_vel_nav/cmd_vel
 	rate = rospy.Rate(HZ) # 50hz
 
+
 	""" Obrot w strone punktu docelowego """
 	message = buildMess(False, True, setRoPo.sign)
 	rospy.loginfo(message)
 	i = 0
-	while i <= HZ*setRoPo.tRot:
+	while round(setRoPo.theta,3) != round(cuRoPo.theta,3):#korekcja z enkoderami
+		calcToPoint(data)
+		message = buildMess(False, True, setRoPo.sign)
 		pub.publish(message)
-		cuRoPo.theta = setRoPo.theta
-		i=i+1
+		print "dest angle: ",setRoPo.theta,"	current angle: ",cuRoPo.theta
 		rate.sleep()
 	
 	""" Zatrzymanie robota w punkcie docelowym """
 	i=0
 	while i<=HZ*2 :
 		message = buildMess(False, False, 1)
-		rospy.loginfo(message)
 		pub.publish(message)
 		i=i+1
 		rate.sleep()
 	
 	""" Jazda w strone punktu docelowego """
-	i=0
+	'''i=0
 	while i<=HZ*setRoPo.tGo:
 		message = buildMess(True, False, 1) # jedziemy zawsze przodem
-		rospy.loginfo(message)
+		if(i==1):
+			rospy.loginfo(message)
+
 		pub.publish(message)
 		i=i+1
-		rate.sleep()
+		rate.sleep()'''
 	
 	""" Zatrzymanie robota w punkcie docelowym """
-	i=0
+	'''i=0
 	while i<=HZ*2 :
 		message = buildMess(False, False, 1)
-		rospy.loginfo(message)
 		pub.publish(message)
 		cuRoPo.x = setRoPo.x
 		cuRoPo.y = setRoPo.y
 		i=i+1
-		rate.sleep()
+		rate.sleep()'''
 
 
 ###------------------------------------------v--MAIN--v------------------------------------------###
