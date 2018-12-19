bacc0d5553c9e7a655ee1a5627dfd86a1a300676
Micha <mlk04@wp.pl>
2018-12-19 23:03:07 +0100

    without_odom

---
+++ scripts/pro_1_without_odom/publisher_without.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# license removed for brevity
+import sys
+import rospy
+from geometry_msgs.msg import Pose
+
+wentThrough = False
+overRange = False
+
+
+def usage(argNum):
+	if (argNum == 2 and sys.argv[1] == "help") or overRange :
+		print "Avaliable modes: "
+		for i in range(6) :
+			print " %s - %s" %(i, modeDesc(i))
+		return
+	
+	if argNum < 4 :
+		print "Not enough arguments given. Usage is like:"
+	else :
+		print "Too much arguments given. Usage is like:"
+	print "	rosrun stero_mobile_init problisher.py x y mode"
+	print "For avaliable modes type in:"
+	print "	rosrun stero_mobile_init problisher.py help"
+
+
+def modeDesc(mode):
+	global x, y
+	
+	if not wentThrough :
+		x = "x"
+		y = "y"
+	
+	if mode == 0 :
+		return "move to position (%s, %s) without feedback correction" %(x,y)
+	elif mode == 1 :
+		return "move to position (%s, %s) with feedback correction" %(x,y)
+	elif mode == 2 :
+		return "go there and back"
+	elif mode == 3 :
+		return "rotate in place about 360 dagrees"
+	elif mode == 4 :
+		return "go square, first corner in position (%s, %s)" %(x,y)
+	elif mode == 5 :
+		return "go square, first corner in position (%s, %s) -> pre-counting" %(x,y)
+
+
+def talker():
+	pub = rospy.Publisher('chatter', Pose, queue_size=10)
+	rospy.init_node('talker', anonymous=True)
+	rate = rospy.Rate(1) # 1hz
+	
+	pose = Pose()
+	pose.position.x = x
+	pose.position.y = y
+	pose.position.z = mode
+	
+	#rospy.loginfo(pose)
+	
+	for i in range(2): # dla pewnosci 2 wiadomosci
+		pub.publish(pose)
+		rate.sleep()
+
+
+if __name__ == '__main__':
+	global x, y, mode
+	
+	argNum = len(sys.argv)
+	
+	if argNum == 4:
+		x = float(sys.argv[1])
+		y = float(sys.argv[2])
+		mode = int(sys.argv[3])
+	else:
+		usage(argNum)
+		sys.exit(1)
+		
+	if mode not in range(6) :
+		overRange = True
+		print "Non-existent mode request"
+		usage(argNum)
+		sys.exit(1)
+	
+	wentThrough = True
+	
+	print "--------------------"
+	print "Requesting mode: (%s) %s" %(mode, modeDesc(mode))
+	print "--------------------"
+	
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
---
+++ scripts/pro_1_without_odom/task_setter_without.py
@@ -0,0 +1,481 @@
+#!/usr/bin/env python
+import sys
+import rospy
+import math
+import PyKDL
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Pose2D
+from nav_msgs.msg import Odometry
+from decimal import Decimal
+
+OMEGA = 2*math.pi/10 # pelny obrot na 30s
+VELOC = 0.7 # predkosc postepowa 0.1
+HZ = 50 # czestotliwsc wysylania wiadomosci
+STATE = -1 # okresla zadanie jakie wykonuje robot (!=0 -> aktualna pozycja z odometrii/lasera)
+erBreFa = VELOC*0.06 # early breaking factor
+working = False # True -> robot podczas pracy -> nie zadasz nowego punktu
+withLaser = True # True -> nawigacja z uzyciem lasera
+lefty = False # True -> kwadrat w lewo / obrot w lewo
+
+
+class Position:
+	"""
+	Pozycja robota na plaszczyznie i kat obrotu (wzgledem pozycji poczatkowej)
+	
+	"""
+	# class attributes (only used for setRoPo's)
+	sign = 1 # kierunek obrotu
+	tRot = 0 # czas obrotu
+	tGo = 0 # czas jazdy
+	s = 0 # droga do przebycia
+
+	# instance attributes
+	def __init__(self, x, y, theta):
+		self.x = x
+		self.y = y
+		self.theta = theta # lewo: (0, PI), prawo: (0, -PI)
+	
+	# instance method
+	def countRoute(self, s):
+		self.s = s
+		self.tGo = s/VELOC
+
+
+cuRoPo = Position(0,0,0) # current Robot Position (x,y,theta) by odometry or laser 
+setRoPo = Position(0,0,0) # zadana Robot Position (x,y,theta)
+hopeRoPo = Position(0,0,0) # hoped Robot Position (x,y,theta) when no feedback 
+
+
+###-----------------------------------------v--CALCS--v-----------------------------------------###
+
+def calcToPoint(destination):
+	"""
+	Wyznacza czas i kierunek obrotu oraz czas jazdy, aby robot osiagnal zadany punkt
+	
+	Parameters
+	----------
+	destination : Pose (or anything that has x and y)
+		punkt do ktorego ma dojechac robot
+	
+	Returns
+	-------
+	setRoPo values
+	
+	"""
+	global working
+	working = True
+	
+	if STATE != 0 :
+		dx = destination.x - cuRoPo.x
+		dy = destination.y - cuRoPo.y
+	else :
+		dx = destination.x - hopeRoPo.x
+		dy = destination.y - hopeRoPo.y
+	
+	if(abs(dx) < 0.1 and abs(dy) < 0.1): # zmniejszyc maksymalnie!!!
+		print "ALREADY IN POINT"
+		working = False
+		return
+	
+	setRoPo.x = round(destination.x, 3)
+	setRoPo.y = round(destination.y, 3)
+	
+	""" Bezwzgledny kat obrotu theta """
+	if(dx != 0):
+		alfa = math.atan(dy/abs(dx))
+		if(dx > 0):
+			theta = alfa
+		else:
+			if(dy > 0):
+				theta = math.pi - alfa
+			else:
+				theta = -math.pi - alfa
+	else:
+		theta = abs(dy)/dy * math.pi/2
+	
+	setRoPo.theta = round(theta, 3)
+	print "Theta:", setRoPo.theta/(math.pi*2)*360, "stopni"
+	
+	""" Wzgledny kat obrotu fi -> czas i kierunek obrotu """
+	if STATE != 0 :
+		fi = setRoPo.theta - cuRoPo.theta # uwzglednienie aktualnego kata obrotu
+	else :
+		fi = setRoPo.theta - hopeRoPo.theta
+	if(abs(fi) > math.pi): # zawsze obracaj sie tak jak masz blizej
+		fi = -(abs(fi)/fi)*(2*math.pi - abs(fi))
+	setRoPo.tRot = abs(fi)/OMEGA
+	setRoPo.sign = fi and (1, -1)[fi < 0] # nie wiem jak to dziala, wzielem z internetu
+	print "Fi:", fi/(math.pi*2)*360, "stopni"
+	
+	#if STATE == 0 or STATE == 4: # zeby nie liczyl bez sensu w przypadku z odometria
+	""" Odleglosc w linii prostej do punktu """
+	setRoPo.countRoute(math.sqrt(dx*dx + dy*dy))
+	print "Do przejechania:", setRoPo.s, "m"
+
+
+def calcNextSquarePoint():
+	"""
+	Wyznacza nastepny punkt ruchu po kwadracie
+	na podstawie aktualnej pozycji cuRoPo
+	
+	Returns
+	-------
+	Position()
+	
+	"""
+	vecRo = PyKDL.Vector(cuRoPo.x, cuRoPo.y, 0)
+	twiRo = PyKDL.Rotation.RPY(0, 0, cuRoPo.theta)
+	mxJednoRo = PyKDL.Frame(twiRo, vecRo)
+	vecDestRel = PyKDL.Vector(0, (lefty*2-1)*setRoPo.s, 0) # kwadrat w lewo gdy lewo==True
+	vecDest = mxJednoRo * vecDestRel
+	nextSqPnt = Position(vecDest[0], vecDest[1], 0)
+	print "nextSqPnt.x:", nextSqPnt.x, "  nextSqPnt.y:", nextSqPnt.y
+	return nextSqPnt
+
+
+def calcAllSquarePoints():
+	"""
+	Wyznacza wszystkie kolejne punkty ruchu po kwadracie
+	na podstawie zadanej pozycji setRoPo wyliczonej wczesniej przez calcToPoint()
+	oraz aktualnej pozycji cuRoPo
+	
+	Returns
+	-------
+	Position() x3
+	
+	"""
+	print "Corner 1: (%s, %s)" %(setRoPo.x, setRoPo.y)
+	
+	""" Tworzenie macierzy jednorodnej robota w setRoPo """
+	vecRo = PyKDL.Vector(setRoPo.x, setRoPo.y, 0)
+	twiRo = PyKDL.Rotation.RPY(0, 0, setRoPo.theta)
+	mxJednoRo = PyKDL.Frame(twiRo, vecRo)
+	
+	vecDestRel = PyKDL.Vector(0, (lefty*2-1)*setRoPo.s, 0) # kwadrat w lewo gdy lewo==True
+	vecDest = mxJednoRo * vecDestRel
+	squarePoint2 = Position(vecDest[0], vecDest[1], 0)
+	print "Corner 2: (%s, %s)" %(round(squarePoint2.x, 3), round(squarePoint2.y, 3))
+	
+	vecDestRel = PyKDL.Vector(-setRoPo.s, (lefty*2-1)*setRoPo.s, 0) # kwadrat w lewo gdy lewo==True
+	vecDest = mxJednoRo * vecDestRel
+	squarePoint3 = Position(vecDest[0], vecDest[1], 0)
+	print "Corner 3: (%s, %s)" %(round(squarePoint3.x, 3), round(squarePoint3.y, 3))
+	
+	squarePoint4 = Position(cuRoPo.x, cuRoPo.y, 0)
+	print "Corner 4: (%s, %s)" %(round(squarePoint4.x, 3), round(squarePoint4.y, 3))
+	
+	return squarePoint2, squarePoint3, squarePoint4
+
+
+###-------------------------------------v--MESS-HANDLERS--v-------------------------------------###
+
+def listener():
+	"""
+	Uruchomienie node'a i nasluchiwanie na topicach:
+	'/chatter'
+	'/elektron/mobile_base_controller/odom'
+	'/pose2D'
+	
+	"""
+	rospy.init_node('listener', anonymous=True)
+	rospy.Subscriber('/chatter', Pose, callback)
+	print "READY TO DO A JOB"
+
+def callback(data):
+	"""
+	Odbiera wiadomosci z punktem zadanym
+	i wywoluje funkcje zarzadzajaca
+	
+	"""
+	rospy.loginfo(rospy.get_caller_id() + "I heard an order: %s", data.position.z)
+	if(not working): manager(data.position)
+
+
+def manager(data):
+	"""
+	Na podstawie wartosci 'z' otrzymanej wiadomosci decyduje jaka akcje wykonac
+	
+	"""
+	global STATE
+	global working
+	
+	if(data.z == 0):
+		if(STATE == 0):return
+		STATE = 0
+		calcToPoint(data) # jesli po wykonaniu tej funkcji working==True, tzn ze przyjal te robote
+		if(working):
+			print "ZACZYNAM RUCH DO PKT"
+			talkerToPoint()
+			working = False
+			STATE = -1
+			
+	elif(data.z == 1):
+		if(STATE == 1):return
+		STATE = 1
+		calcToPoint(data) # jesli po wykonaniu tej funkcji working==True, tzn ze przyjal te robote
+		if(working):
+			print "ZACZYNAM RUCH DO PKT I Z POWROTEM"
+			talkerToSlide()
+			working = False
+			STATE = -1
+			
+	elif(data.z == 2):
+		if(STATE == 2):return
+		STATE = 2
+		print "ZACZYNAM TEST ~OBROT~"
+		talkerToTurn()
+		working = False
+		STATE = -1
+
+	elif(data.z == 3):
+		if(STATE == 3):return
+		STATE = 3
+		calcToPoint(data) # jesli po wykonaniu tej funkcji working==True, tzn ze przyjal te robote
+		if(working):
+			print "ZACZYNAM RUCH KWADRAT"
+			talkerToPoint()
+			cuRoPo.x = hopeRoPo.x
+			cuRoPo.y = hopeRoPo.y
+			cuRoPo.theta = hopeRoPo.theta
+			[msg1,msg2,msg3] = calcAllSquarePoints()
+			pose = Pose()
+			pose.position.x = msg1.x
+			pose.position.y = msg1.y
+			calcToPoint(pose.position)
+			talkerToPoint()
+			pose.position.x = msg2.x
+			pose.position.y = msg2.y
+			calcToPoint(pose.position)
+			talkerToPoint()
+			pose.position.x = msg3.x
+			pose.position.y = msg3.y
+			calcToPoint(pose.position)
+			talkerToPoint()
+			working = False
+			STATE = -1
+
+	else :
+		print "Non-existent mode request"
+
+
+	"""	
+	elif(data.z == 3):
+		if(STATE == 3): return # nie rob kwadratu drugi raz z rzedu
+		STATE = 3
+		calcToPoint(data) # jesli po wykonaniu tej funkcji working==True, tzn ze przyjal te robote
+		if(working):
+			print "ZACZYNAM TEST ~RUCH PO KWADRACIE~ #ODOM"
+			talkerToPointOdom()
+			for i in range(3):
+				nextPoint = calcNextSquarePoint()
+				calcToPoint(nextPoint)
+				talkerToPointOdom()
+			working = False
+	"""		
+	
+
+###-------------------------------------v--MESS-BUILDERS--v-------------------------------------###
+
+def buildMess(go, rot, sign):
+	"""
+	Buduje wiadomosc przekazywana do robota
+
+	Parameters
+	----------
+	go : bool
+		if True - zadajemy predkosc postepowa
+	rot : bool
+		if True - zadajemy predkosc obrotowa
+	sign : 1/-1
+		okresla kierunek obrotu (lewo/prawo) i jazdy (przod/tyl)
+
+	Returns
+	-------
+	Twist()
+	
+	"""
+	vel_msg = Twist()
+	vel_msg.linear.y = 0
+	vel_msg.linear.x = VELOC*go*sign
+	vel_msg.angular.x = 0
+	vel_msg.angular.y = 0
+	vel_msg.angular.z = OMEGA*rot*sign
+	#print "predkosc obrotowa:", vel_msg.angular.z
+	return vel_msg
+
+
+###----------------------------------------v--TALKERS--v----------------------------------------###
+
+def talkerToPoint():
+	"""
+	Publikuje polecenia do robota,
+	majace na celu jego przemieszczenie do zadanego punktu
+	nie uwzglednia odometrii
+	
+	"""
+	print "talkerToPoint STARTED"
+	pub = rospy.Publisher('/mux_vel_nav/cmd_vel', Twist, queue_size=10) # TOPIC: /mux_vel_nav/cmd_vel
+	rate = rospy.Rate(HZ)
+	
+	""" Obrot w strone punktu docelowego """
+	message = buildMess(False, True, setRoPo.sign)
+	#rospy.loginfo(message)
+	i=0
+	while i <= HZ*setRoPo.tRot:
+		pub.publish(message)
+		print "\rdest angle: ", format(setRoPo.theta,'.3f'), " current angle: ", format(cuRoPo.theta,'.3f'),
+		sys.stdout.flush()
+		i=i+1
+		rate.sleep()
+	
+	""" Zatrzymanie robota w punkcie docelowym """
+	message = buildMess(False, False, 1)
+	#rospy.loginfo(message)
+	i=0
+	while i<=HZ :
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+		
+	hopeRoPo.theta = setRoPo.theta
+	
+	""" Jazda w strone punktu docelowego """
+	message = buildMess(True, False, 1) # jedziemy zawsze przodem
+	#rospy.loginfo(message)
+	i=0
+	while i<=HZ*setRoPo.tGo:
+		pub.publish(message)
+		print "dest x: ", format(setRoPo.x,'.3f'), "  current x: ", format(cuRoPo.x,'.3f')
+		print "dest y: ", format(setRoPo.y,'.3f'), "  current y: ", format(cuRoPo.y,'.3f')
+		print "--------"
+		i=i+1
+		rate.sleep()
+	
+	""" Zatrzymanie robota w punkcie docelowym """
+	message = buildMess(False, False, 1)
+	#rospy.loginfo(message)
+	i=0
+	while i<=HZ :
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+	
+	hopeRoPo.x = setRoPo.x
+	hopeRoPo.y = setRoPo.y
+	
+	print "\ntalkerToPoint FINISHED with:"
+	print "	dest angle: ", format(hopeRoPo.theta,'.3f'), " current angle: ", format(cuRoPo.theta,'.3f')
+	print "	dest x: ", format(hopeRoPo.x,'.3f'), "  current x: ", format(cuRoPo.x,'.3f')
+	print "	dest y: ", format(hopeRoPo.y,'.3f'), "  current y: ", format(cuRoPo.y,'.3f')
+	print "\n"
+
+
+def talkerToSlide():
+	"""
+	Publikuje polecenia do robota,
+	majace na celu jego przejechanie w przod i w tyl
+	uwzglednia odometrie
+	
+	Parameters ?
+	----------
+	setRoPo : Position()
+		pozycja zadana dla robota
+	
+	"""
+	print "talkerToSlide STARTED"
+	pub = rospy.Publisher('/mux_vel_nav/cmd_vel', Twist, queue_size=10) # TOPIC: /mux_vel_nav/cmd_vel
+	rate = rospy.Rate(HZ)
+	
+	""" Obrot w strone punktu docelowego """
+	message = buildMess(False, True, setRoPo.sign)
+	#rospy.loginfo(message)
+	i=0
+	while i <= HZ*setRoPo.tRot:
+		pub.publish(message)
+		print "\rdest angle: ", format(setRoPo.theta,'.3f'), " current angle: ", format(cuRoPo.theta,'.3f'),
+		sys.stdout.flush()
+		i=i+1
+		rate.sleep()
+	
+	""" Zatrzymanie robota w punkcie docelowym """
+	message = buildMess(False, False, 1)
+	#rospy.loginfo(message)
+	i=0
+	while i<=HZ :
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+		
+	hopeRoPo.theta = setRoPo.theta
+	
+	""" Jazda w strone punktu docelowego """
+	message = buildMess(True, False, 1) # jedziemy zawsze przodem
+
+	i=0
+	while i<=HZ*setRoPo.tGo:
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+	
+	""" Zatrzymanie robota w punkcie docelowym """
+	message = buildMess(False, False, 1)
+
+	i=0
+	while i<=HZ :
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+	
+	hopeRoPo.x = setRoPo.x
+	hopeRoPo.y = setRoPo.y
+
+	message = buildMess(True, False, -1)
+	i=0
+	while i <= HZ*setRoPo.tGo:
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+
+	""" Zatrzymanie robota w punkcie docelowym """
+	message = buildMess(False, False, 1)
+
+	i=0
+	while i<=HZ :
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+
+def talkerToTurn():
+	print "talkerToTurn STARTED"
+	pub = rospy.Publisher('/mux_vel_nav/cmd_vel', Twist, queue_size=10) # TOPIC: /mux_vel_nav/cmd_vel
+	rate = rospy.Rate(HZ)
+	
+	""" Obrot o 360 """
+	message = buildMess(False, True, 1)
+	i=0
+	while i <= HZ*(6.28/OMEGA):
+		pub.publish(message)
+		sys.stdout.flush()
+		i=i+1
+		rate.sleep()
+	
+	""" Zatrzymanie robota w punkcie docelowym """
+	message = buildMess(False, False, 1)
+	#rospy.loginfo(message)
+	i=0
+	while i<=HZ :
+		pub.publish(message)
+		i=i+1
+		rate.sleep()
+
+	hopeRoPo.x = setRoPo.x
+	hopeRoPo.y = setRoPo.y
+
+###------------------------------------------v--MAIN--v------------------------------------------###
+
+if __name__ == '__main__':
+	listener()
+	rospy.spin()
+	#while not rospy.is_shutdown():
+		
