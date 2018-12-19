180cc88f038027e9d153284afe83678204b162c2
Micha <mlk04@wp.pl>
2018-12-20 00:55:14 +0100

    no odom end

--- matlab/proj3_without_odom/diff_drive/reading_data.m
+++ matlab/proj3_without_odom/diff_drive/reading_data.m
@@ -1,42 +1,42 @@
 %błąd kwadratu
-% bag = rosbag('proj3_diff_drive_square_error.bag');
-% 
-% %błąd
-% bSel1 = select(bag,"Topic",'/error');
-% msgStructs1 = readMessages(bSel1);
-% x_error = cellfun(@(m) double(m.X),msgStructs1);
-% y_error = cellfun(@(m) double(m.Y),msgStructs1);
-% theta_error = cellfun(@(m) double(m.Theta),msgStructs1);
-% 
-% error = (x_error.^2 + y_error.^2).^(0.5)
-% 
-% figure(1)
-% plot(error)
-% xlim([0 length(error)]);
-% xlabel('time');
-% ylabel('path error');
-% title(['Błąd kwadratu diffdrive'])
-% print ('square_path_error', '-dpng', '-r400')
-%         
-% figure(2)   
-% hold on;
-% title(['Błąd kwadratu diffdrive'])
-% plot(theta_error);
-% xlabel('time');
-% ylabel('theta error');
-% print ('square_theta_error', '-dpng', '-r400')
-% 
-% %ścieżka
-% bSel2 = select(bag,"Topic",'/gazebo_odom');
-% msgStructs2 = readMessages(bSel2);
-% x = cellfun(@(m) double(m.Pose.Pose.Position.X),msgStructs2);
-% y = cellfun(@(m) double(m.Pose.Pose.Position.Y),msgStructs2);
-% figure(3)
-% plot(x,y)
-% title(['Trasa kwadrat']);
-% xlabel('x');
-% ylabel('y');
-% print ('square_path', '-dpng', '-r400')
+bag = rosbag('proj3_diff_drive_square_error.bag');
+
+%błąd
+bSel1 = select(bag,"Topic",'/error');
+msgStructs1 = readMessages(bSel1);
+x_error = cellfun(@(m) double(m.X),msgStructs1);
+y_error = cellfun(@(m) double(m.Y),msgStructs1);
+theta_error = cellfun(@(m) double(m.Theta),msgStructs1);
+
+error = (x_error.^2 + y_error.^2).^(0.5)
+
+figure(1)
+plot(error)
+xlim([0 length(error)]);
+xlabel('time');
+ylabel('path error');
+title(['Błąd kwadratu diffdrive'])
+print ('square_path_error', '-dpng', '-r400')
+        
+figure(2)   
+hold on;
+title(['Błąd kwadratu diffdrive'])
+plot(theta_error);
+xlabel('time');
+ylabel('theta error');
+print ('square_theta_error', '-dpng', '-r400')
+
+%ścieżka
+bSel2 = select(bag,"Topic",'/gazebo_odom');
+msgStructs2 = readMessages(bSel2);
+x = cellfun(@(m) double(m.Pose.Pose.Position.X),msgStructs2);
+y = cellfun(@(m) double(m.Pose.Pose.Position.Y),msgStructs2);
+figure(3)
+plot(x,y)
+title(['Trasa kwadrat']);
+xlabel('x');
+ylabel('y');
+print ('square_path', '-dpng', '-r400')
 
 %błąd ruchu naprzód i spwerotem
 bag = rosbag('proj3_diff_drive_slide_error.bag');
