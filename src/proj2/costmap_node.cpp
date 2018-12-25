60fe537b1e2bc81dff7575ce9bd4af5ada637064
Micha <mlk04@wp.pl>
2018-12-25 14:33:24 +0100

    params changed

--- src/proj2/costmap_node.cpp
+++ src/proj2/costmap_node.cpp
@@ -11,14 +11,17 @@
 
 int main(int argc, char** argv){
 
-	ros::init(argc,argv,"publish_multiple_goals");
+	std::cout<<"IT WORKS !"<<std::endl;
+	ros::init(argc,argv,"publish_costmap");
 	ros::NodeHandle n;
-
+	std::cout<<"IT WORKS 0!"<<std::endl;
 	//tf::TransformListener tf(ros::Duration(10));
 	tf2_ros::Buffer buffer(ros::Duration(10),true);
+	std::cout<<"IT WORKS 1!"<<std::endl;
 	buffer.setUsingDedicatedThread(true);//bo tf jest publikowany przez inny watek
-	costmap_2d::Costmap2DROS costmap("costmap_node", buffer);
-	std::cout<<"IT WORKS!"<<std::endl;
-
+	std::cout<<"IT WORKS 2!"<<std::endl;
+	costmap_2d::Costmap2DROS costmap("costmap", buffer);
+	//costmap_2d::Costmap2DPublisher publisher(&n,costmap.getCostmap(),"map","/costmap",true);
+	//publisher.publishCostmap();
     return 0;
 }
